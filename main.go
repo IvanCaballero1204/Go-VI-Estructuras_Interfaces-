4395135dc44165caa9acefd2210cb13aeddac5a7
Gabriel Ivan Gradilla Caballero <gabriel.ivan.gradilla.caballero1@ibm.com>
2020-10-28 02:15:30 -0600

    Terminado

--- main.go
+++ main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"./multimedia"
+)
 
 func main(){
+	var stop bool = false
+	var opc int
+	imagen := new(multimedia.Imagen)
+	audio := new(multimedia.Audio)
+	video := new(multimedia.Video)
+	contenidoWeb := new(multimedia.ContenidoWeb)
+
+	for !stop {
+		fmt.Println(":::MENU:::")
+		fmt.Println("1) AGREGAR IMAGEN")
+		fmt.Println("2) AGREGAR AUDIO")
+		fmt.Println("3) AGREGAR VIDEO")
+		fmt.Println("4) MOSTRAR")
+		fmt.Println("5) SALIR")
+		fmt.Print("OPCION (1-5): ")
+		fmt.Scan(&opc)
 	
+		switch opc {
+			case 1:
+				input := ""
+				auxFloat := 0.0
+		
+				fmt.Print("Titulo: ")
+				fmt.Scan(&input)
+				imagen.Titulo = input
+				fmt.Print("Formato (jpg, gif, png): ")
+				fmt.Scan(&input)
+				imagen.Formato = input
+				fmt.Print("Canal: ")
+				fmt.Scan(&auxFloat)
+				imagen.Canal = auxFloat
+				contenidoWeb.ContenidoMultimedia = append(contenidoWeb.ContenidoMultimedia, imagen)
+			case 2:
+				input := ""
+				auxInt := 0
+		
+				fmt.Print("Titulo: ")
+				fmt.Scan(&input)
+				audio.Titulo = input
+				fmt.Print("Formato (mp3, wma, wav): ")
+				fmt.Scan(&input)
+				audio.Formato = input
+				fmt.Print("Duracion: ")
+				fmt.Scan(&auxInt)
+				audio.Duracion = auxInt
+				contenidoWeb.ContenidoMultimedia = append(contenidoWeb.ContenidoMultimedia, audio)
+			case 3:
+				input := ""
+				auxInt := 0
+		
+				fmt.Print("Titulo: ")
+				fmt.Scan(&input)
+				video.Titulo = input
+				fmt.Print("Formato (mp4, wmv, mov): ")
+				fmt.Scan(&input)
+				video.Formato = input
+				fmt.Print("Frames: ")
+				fmt.Scan(&auxInt)
+				video.Frames = auxInt
+				contenidoWeb.ContenidoMultimedia = append(contenidoWeb.ContenidoMultimedia, video)
+			case 4:
+				fmt.Print(contenidoWeb.Mostrar())
+			case 5:
+				stop = true
+			default:
+				fmt.Println("ERROR OPCION NO VALIDA")
+		}
+	}
 }
--- multimedia/multimedia.go
+++ multimedia/multimedia.go
@@ -1,2 +1,59 @@
 package multimedia
 
+import "strconv"
+
+type Multimedia interface {
+	Mostrar() string
+}
+
+type ContenidoWeb struct {
+	ContenidoMultimedia []Multimedia
+}
+
+func (cw *ContenidoWeb) Mostrar() string {
+	var auxWeb string
+	for _, contenido := range cw.ContenidoMultimedia {
+		auxWeb += contenido.Mostrar() + "\n"
+	}
+
+	return auxWeb
+}
+
+type Imagen struct {
+	Titulo, Formato string 
+	Canal float64
+}
+
+func (i *Imagen) Mostrar() string {
+	auxCadena := ""
+
+	auxCadena += "Image Name: " + i.Titulo + "." + i.Formato + ", Channels: " + strconv.FormatFloat(i.Canal, 'f', 6, 64)
+	
+	return auxCadena
+}
+
+type Audio struct {
+	Titulo, Formato string 
+	Duracion int
+}
+
+func (a *Audio) Mostrar() string {
+	auxCadena := ""
+
+	auxCadena += "Track: " + a.Titulo + "." + a.Formato + ", Duration: " + strconv.Itoa(a.Duracion) + " segs"
+	
+	return auxCadena
+}
+
+type Video struct {
+	Titulo, Formato string
+	Frames int
+}
+
+func (v *Video) Mostrar() string {
+	auxCadena := ""
+
+	auxCadena += "Video: " + v.Titulo + "." + v.Formato + ", Frames: " + strconv.Itoa(v.Frames)
+	
+	return auxCadena
+}
